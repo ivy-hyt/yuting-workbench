a81e3b11876416ecdf8f570d6bbabd08e6d42af9
Yuting Workbench <yuting@local>
2026-07-31 12:02:46 +0800

    fix: 统一 HealthKit 插件 SPM 包名为 YutingHealthkitBridge

--- plugins/healthkit-bridge/Package.swift
+++ plugins/healthkit-bridge/Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "HealthKitBridge",
+    name: "YutingHealthkitBridge",
     platforms: [
         .iOS(.v13)
     ],
     products: [
-        .library(name: "HealthKitBridge", targets: ["HealthKitBridge"])
+        .library(name: "YutingHealthkitBridge", targets: ["YutingHealthkitBridge"])
     ],
     targets: [
         .target(
-            name: "HealthKitBridge",
+            name: "YutingHealthkitBridge",
             path: "ios",
             linkerSettings: [
                 .linkedFramework("HealthKit")
