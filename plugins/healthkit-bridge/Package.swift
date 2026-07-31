6442f7d7ac1e691345516e65d1b93cbd332982c8
Yuting Workbench <yuting@local>
2026-07-31 14:18:24 +0800

    fix: 插件部署目标对齐 iOS15 + CI 编译关闭 entitlements

--- plugins/healthkit-bridge/Package.swift
+++ plugins/healthkit-bridge/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "YutingHealthkitBridge",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "YutingHealthkitBridge", targets: ["YutingHealthkitBridge"])
