b314c91115944c177813e3d7c426a1cf9fc6cb0b
Yuting Workbench <yuting@local>
2026-07-31 14:07:35 +0800

    fix: HealthKit 插件 SPM 补充 Capacitor 依赖

--- plugins/healthkit-bridge/Package.swift
+++ plugins/healthkit-bridge/Package.swift
@@ -9,9 +9,15 @@ let package = Package(
     products: [
         .library(name: "YutingHealthkitBridge", targets: ["YutingHealthkitBridge"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+    ],
     targets: [
         .target(
             name: "YutingHealthkitBridge",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm")
+            ],
             path: "ios",
             linkerSettings: [
                 .linkedFramework("HealthKit")
